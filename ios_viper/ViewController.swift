48a42c264e38e7f9b4397260b201899b92c5fbd9
Khurshid <shamsun.com@gmail.com>
2019-11-06 16:11:41 +0900

    Initial Commit

---
+++ ios_viper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_viper
+//
+//  Created by KHURSHIDBEK on 2019/11/06.
+//  Copyright © 2019 UHanaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
